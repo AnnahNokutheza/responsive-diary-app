65129fb4fb0a9f0585946f7408141909908e4e79
AnnahNokutheza <annahmkhwanazi18@gmail.com>
2023-09-05 16:09:01 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'; // Import the styled-components library
+import styled from 'styled-components';
 import './styles/main.css';
 import Header from './components/Header';
 import DiaryList from './components/DiaryList';
 import EntryForm from './components/EntryForm';
+
+// Import your cover image here
+import coverImage from './images/to/cover-image.jpg';
 
 const Container = styled.div`
   width: 90%;
@@ -13,6 +16,13 @@ const Container = styled.div`
   @media (max-width: 768px) {
     width: 95%;
   }
+`;
+
+const CoverImage = styled.img`
+  width: 100%;
+  max-height: 300px; /* Adjust the height as needed */
+  object-fit: cover; /* Maintain aspect ratio and cover the container */
+  margin-bottom: 1rem; /* Add some spacing between the cover image and other content */
 `;
 
 const App = () => {
@@ -30,6 +40,8 @@ const App = () => {
   return (
     <Container>
       <Header />
+      <CoverImage src={coverImage} alt="Diary Cover" />
+
       <EntryForm onAddEntry={handleAddEntry} />
       <DiaryList entries={entries} />
     </Container>
